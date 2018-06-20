74451cc3ded7c381aafd235a7034bb8aee38b663
Klochenok Roman <r.klochenok@gmail.com>
2018-06-20 23:12:32 +0300

    Initial commit

---
+++ telegram_bot/tasks.py
@@ -0,0 +1,18 @@
+import requests
+from celery import task
+from celery.utils.log import get_task_logger
+from django.conf import settings
+from datetime import datetime
+
+
+logger = get_task_logger(__name__)
+
+@task
+def send_message_to_telegram_task():
+    chat_id = settings.TELEGRAM_CHAT_ID
+    token = settings.TELEGRAM_NOTIFICATIONS_TOKEN
+    message = datetime.now().time().strftime("%H:%M")
+    url = "https://api.telegram.org/bot{}/sendMessage".format(token)
+    r = requests.post(url, data={'chat_id': chat_id, 'text': message})
+    r.raise_for_status()
+    logger.info('Sent message to telegram: {}'.format(message))
